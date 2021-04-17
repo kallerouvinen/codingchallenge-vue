88b2b158d2ee407d38767ade2534be4aafaeff9d
kaller <kalle_rouvinen@hotmail.com>
2021-04-17 12:25:29 +0300

    Add some initial mock data and connect Vuex store to app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from "vue";
+import Vuex from "vuex";
+
 import App from "./App.vue";
+import { items } from "./mockdata";
 
 Vue.config.productionTip = false;
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    items,
+  },
+  mutations: {
+    testMutation(state, item) {
+      console.log(item);
+    },
+  },
+});
 
 new Vue({
   render: (h) => h(App),
+  store,
 }).$mount("#app");
