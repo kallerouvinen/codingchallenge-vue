731276d8e98ab53b559f3850c8d275000af46cd8
kaller <kalle_rouvinen@hotmail.com>
2021-04-17 18:05:55 +0300

    Get data from fake API when ProductList is created, also add skeleton component as loader

---
+++ src/components/ProductCardSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "ProductCardSkeleton",
+};
+</script>
+
+<style scoped>
+div {
+  width: 200px;
+  height: 300px;
+  box-shadow: 0px 3px 16px 8px rgba(0, 0, 0, 0.2);
+  margin: 16px;
+  border-radius: 8px;
+  overflow: hidden;
+  background-color: #e6e6e6;
+}
+</style>
--- src/routes/ProductList.vue
+++ src/routes/ProductList.vue
@@ -1,21 +1,59 @@
 <template>
-  <ul>
-    <ProductCard v-for="i in items" v-bind:key="i.id" v-bind:item="i" />
-  </ul>
+  <div>
+    <h3 v-if="error">Oopsie! Something went wrong!</h3>
+    <div class="skeleton" v-if="loading">
+      <ProductCardSkeleton
+        v-for="i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
+        v-bind:key="i.id"
+      />
+    </div>
+    <ul v-if="items">
+      <ProductCard v-for="i in items" v-bind:key="i.id" v-bind:item="i" />
+    </ul>
+  </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapMutations, mapState } from "vuex";
 import ProductCard from "../components/ProductCard.vue";
+import ProductCardSkeleton from "../components/ProductCardSkeleton.vue";
 
 export default {
   name: "ProductList",
   components: {
     ProductCard,
+    ProductCardSkeleton,
   },
   computed: mapState({
     items: (state) => state.items,
   }),
+  created() {
+    this.getProducts();
+  },
+  data() {
+    return {
+      error: null,
+      loading: false,
+    };
+  },
+  methods: {
+    ...mapMutations(["setItems"]),
+    async getProducts() {
+      this.error = null;
+      this.loading = true;
+
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/photos?albumId=1"
+        );
+        const data = await response.json();
+        this.setItems(data);
+      } catch (error) {
+        this.error = error.response;
+      }
+      this.loading = false;
+    },
+  },
 };
 </script>
 
@@ -26,6 +64,13 @@ ul {
   flex-direction: row;
   flex-wrap: wrap;
   list-style-type: none;
+  padding: 16px 8px;
+}
+.skeleton {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
   padding: 16px 8px;
 }
 </style>
